da26f13aa62537a780e089f95443fedb26367f02
if-loop69420 <j.sztavi@pm.me>
2023-09-11 10:35:01 +0200

    Working flake. Flake-utils suck

--- flake.nix
+++ flake.nix
@@ -4,20 +4,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, nixpkgs, rust-overlay, flake-utils}:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let 
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [rust-overlay.overlays.default];
-          };
-          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
-        in {
-          devShells.${system}.default = pkgs.callPackage ./shell.nix { inherit pkgs; toolchain = toolchain;}; 
-        }
-      );
+  outputs = {self, nixpkgs, rust-overlay,}:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.callPackage ./shell.nix { inherit pkgs; toolchain = toolchain; };
+  };
 }
