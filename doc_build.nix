e40918a4cdee23a07b000301ee58d6bc3a35e4ed
if-loop69420 <j.sztavi@pm.me>
2023-09-20 16:31:34 +0200

    Changed doc_build.nix to pull latest from main

--- doc_build.nix
+++ doc_build.nix
@@ -3,11 +3,9 @@ with pkgs;
 stdenv.mkDerivation rec {
   name = "RECT-DiplomaThesis";
   
-  src = fetchFromGitHub {
-    owner = "F-WuTS";
-    repo = "RECT-Diploma-Thesis";
-    rev = "35fc4a0afbc90922a1beaf95b6a91ea0e404fc87";
-    sha256 = "sha256-WMtqIQrSBxJ1LH6ZPSRt7hvCx9qiP21YxIcJOSr2F7w=";
+  src = fetchGit {
+    url = "git@github.com:if-loop69420/rect-dev-flake.git";
+    ref = "main";
   };
 
   buildInputs = [
