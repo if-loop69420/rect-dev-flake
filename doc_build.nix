2d79bedfd035dd307db9363552622b3cd0d8bb32
if-loop69420 <j.sztavi@pm.me>
2023-09-20 16:26:15 +0200

    Changed revision

--- doc_build.nix
+++ doc_build.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "F-WuTS";
     repo = "RECT-Diploma-Thesis";
-    rev = "7ccc5e178be122d59bb9818963be0bfdf315b368";
+    rev = "35fc4a0afbc90922a1beaf95b6a91ea0e404fc87";
     sha256 = "sha256-WMtqIQrSBxJ1LH6ZPSRt7hvCx9qiP21YxIcJOSr2F7w=";
   };
 
