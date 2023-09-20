f47e9b9a3080593da96885229ed06368edb294c9
if-loop69420 <j.sztavi@pm.me>
2023-09-20 16:56:17 +0200

    Now fetching a zip file instead of a git repo.

--- doc_build.nix
+++ doc_build.nix
@@ -3,16 +3,20 @@ with pkgs;
 stdenv.mkDerivation rec {
   name = "RECT-DiplomaThesis";
   
-  src = fetchGit {
-    url = "git@github.com:if-loop69420/rect-dev-flake.git";
-    ref = "main";
-  };
+  src = fetchurl {
+    url = "https://github.com/F-WuTS/RECT-Diploma-Thesis/archive/refs/heads/master.zip";
+    sha256 = "sha256-Q2DBJYUyVQYvz7AR9J4JxriuZp/P2SId6fOCJDU0y3A=";
+  }; 
 
   buildInputs = [
+    unzip
     texlive.combined.scheme-full
   ];
 
+  unpackPhase = ""; 
+
   buildPhase = '' 
+    ls
     latexmk -pdf main.tex  
   ''; 
 
